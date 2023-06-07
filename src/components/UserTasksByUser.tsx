408e8134c2fec744bfd19bd1a27734d57ebb7b50
trajkovicsj <sakitrajkovic.saki@gmail.com>
2023-06-07 13:12:38 +0200

    delete task function added

--- src/components/UserTasksByUser.tsx
+++ src/components/UserTasksByUser.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import './styles.css'
-import { useLocation } from 'react-router-dom';
+import { redirect, useLocation, useNavigate } from 'react-router-dom';
 import axiosInstance from '../axios';
 
 function UserTasksByUser() {
     const location = useLocation();
+    const navigate = useNavigate()
     const from = location.state?.from?.pathname || "/";
 
     const [tasks, setTasks] = useState<any[]>([{}]);
@@ -23,6 +24,19 @@ function UserTasksByUser() {
             .catch(setError);
     }, []);
 
+    const deleteRow = (id: any) => {
+        console.log(id)
+        try {
+            const response = axiosInstance.delete('/task/delete-task' + id)
+            console.log(response)
+            alert('Task successfully deleted')
+            window.location.reload()
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className='main'>
             <h1 className='heading'>Tasks:</h1>
@@ -32,12 +46,15 @@ function UserTasksByUser() {
                         <div className='elements' key={i}>
                             <table align='center'>
                                 <tr>
+                                    <th>Id task</th>
                                     <th>Task Description</th>
                                     <th>Done</th>
                                 </tr>
                                 <tr>
+                                    <td>{task.idTodoItems}</td>
                                     <td>{task.taskDescription}</td>
-                                    <td>{task.done}</td></tr>
+                                    <td>{task.done}</td>
+                                    <td><button onClick={() => deleteRow(task.idTodoItems)}>Delete</button></td></tr>
                             </table>
                         </div>
                     )
